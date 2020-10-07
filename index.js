ad6b9e5aa5cde3a44b0711a279f5b42e1fe29f53
github username <github email address>
2020-10-07 10:29:05 -0400

    all done

--- index.js
+++ index.js
@@ -3,4 +3,47 @@ document.addEventListener("DOMContentLoaded", function(){
   renderBot(currentPosition)
 
   // ADD CODE HERE!
+  document.addEventListener("keydown", function(e) {
+    // console.log(e.key)
+    const uL = document.getElementById("moves-container")
+    const newLi = document.createElement("li")
+
+    if (e.key == "ArrowRight") {
+      uL.appendChild(newLi)
+      newLi.innerHTML = "right"
+      
+    }else if (e.key == "ArrowLeft") {
+      uL.appendChild(newLi)
+      newLi.innerHTML = "left" 
+  
+    }else if (e.key == "ArrowUp") {
+      uL.appendChild(newLi)
+      newLi.innerHTML = "up" 
+  
+    }else if (e.key == "ArrowDown") {
+      uL.appendChild(newLi)
+      newLi.innerHTML = "down" 
+  
+    }else 
+    
+    {console.log("Nice Try!")
+  }
+
+  })
+  const moveButton = document.getElementById("move-button")
+  moveButton.addEventListener("click", function(e) {
+// console.log("pressed")
+    const btParent = moveButton.parentElement
+    const moveDirection = btParent.querySelector("li")
+    // console.log(moveDirection)
+    // console.dir(moveDirection)
+    move(moveDirection.innerText)
+    moveDirection.remove()
+  })
+
+
+
+  
+
+
 })
